524a2059a85019308aae933018902f701c2137a0
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-05-10 22:23:19 +0100

    amend test page

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
-import styles from "../styles/About.module.css"
 
 const About: NextPage = () => {
-    return <div className={styles.container}>About Page</div>
+    return <div>About Page</div>
 }
 
 export default About
