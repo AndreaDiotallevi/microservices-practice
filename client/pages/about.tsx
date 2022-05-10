9579c1c4664e6b4bf71a393caa01c1da812589fc
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-05-10 22:21:21 +0100

    initialise next project

---
+++ client/pages/about.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/About.module.css"
+
+const About: NextPage = () => {
+    return <div className={styles.container}>About Page</div>
+}
+
+export default About
