f987a966f6deb1b9e486b1c38966b333870d71a5
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-05-14 10:42:51 +0100

    modify about page

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -3,7 +3,7 @@ import Head from "next/head"
 import Image from "next/image"
 
 const About: NextPage = () => {
-    return <div>About Page</div>
+    return <div>About Page Test 4</div>
 }
 
 export default About
