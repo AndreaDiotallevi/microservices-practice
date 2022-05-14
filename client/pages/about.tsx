28e81bbbae7425c81dcc8507b94e49393dc85d19
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-05-14 14:40:53 +0100

    install apollo client and react-intl

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -3,7 +3,7 @@ import Head from "next/head"
 import Image from "next/image"
 
 const About: NextPage = () => {
-    return <div>About Page Test 4</div>
+    return <div>About Page Test 7</div>
 }
 
 export default About
