172f31efee8aec819ecae7d12f3ba1db0bdcaea6
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-05-17 22:02:36 +0100

    test getInitialProps

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,9 +1,31 @@
 import type { NextPage } from "next"
+import { gql, useQuery } from "@apollo/client"
+import client from "../apollo-client"
 import Head from "next/head"
 import Image from "next/image"
 
-const About: NextPage = () => {
-    return <div>About Page Test 7</div>
+const About: NextPage = (props) => {
+    console.log(props)
+
+    // const { data } = useQuery(gql`
+    //     query Hello {
+    //         hello
+    //     }
+    // `)
+
+    return <div>About Page</div>
 }
 
 export default About
+
+About.getInitialProps = async () => {
+    const { data } = await client.query({
+        query: gql`
+            query Hello {
+                hello
+            }
+        `,
+    })
+
+    return { hello: data.hello }
+}
